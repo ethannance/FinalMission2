aee0f165fbe32e33f22eced9b2f9eda42f50bf68
ethannance <ethannance@ETHANNANCE8686>
2024-01-18 16:09:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//Ethan Nance Section 04 group 11
+//program that rolls dice 
+
+namespace FinalMission2;
+
+//Create a second class, I just called it Other
+class Messages
+{
+    //Create main class
+    public static void Main(string[] args)
+    {
+        //Welcomes and gathers input from user
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many dice rolls would you like to simulate? ");
+
+        //Takes input from keyboard above
+        int HowManyRolls = int.Parse(Console.ReadLine());
+
+        //This passes HowManyrolls into the RollDiceMethod, and stores the value that the method is returning (rollTotals)
+        //into a new varaible called rollTotalsReceived
+        int[] rollTotalsReceived = RollDice.RollDiceMethod(HowManyRolls);
+
+        //Write helpful information
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {HowManyRolls}.");
+
+        //Get percentage of how many times each total happened by accessing positions in array
+        for (int iCount2 = 0; iCount2 < 11; iCount2++)
+        {
+            //Calculate percentages for each total
+            int percentage = (int)((double)rollTotalsReceived[iCount2] / HowManyRolls * 100);
+            Console.Write((iCount2 + 2) + ": ");
+
+            //Print the same number of * as there was percentage ints in each position of array
+            for (int iCount3 = 0; iCount3 < percentage; iCount3++)
+            {
+                Console.Write("*");
+            }
+
+            //Moves to new line
+            Console.WriteLine();
+        }
+        //bye bye
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
+
+
+
---
+++ RollDice.cs
@@ -0,0 +1,47 @@
+﻿//Ethan Nance Section 04 group 11
+//program that rolls dice 
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalMission2
+{
+    //Create the RollDice class
+    class RollDice
+    {
+        //Create the RollDicemethod that receives HowManyRolls as a parameter
+        public static int[] RollDiceMethod(int HowManyRolls)
+        {
+            //Initialize dice and total
+            int die1 = 0;
+            int die2 = 0;
+            int total = 0;
+
+            //Create random 
+            Random random = new Random();
+
+            //Create rollTtoals array
+            int[] rollTotals = new int[13];
+
+            //For every time the user said to roll the dice, roll them and store the count of each total in the rollTotals array
+            for (int iCount = 0; iCount < HowManyRolls; iCount++)
+            {
+                //Geenrate random number for each dice, and add to total
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                total = die1 + die2;
+
+                //Keep track of how often each total is rolled
+                rollTotals[total - 2]++;
+            }
+
+            //This is what the method is returning, this array
+            return rollTotals;
+
+        }
+    }
+}
